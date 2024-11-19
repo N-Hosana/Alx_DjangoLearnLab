117fb52cb034d3c8af050ab37067cf892af4509c
N-Hosana <hosanah08@gmail.com>
2024-11-20 01:01:03 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include  # Ensure `include` is imported
+from .views import BookList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Ensure this line includes `api.urls`
+    path('books/', BookList.as_view(), name='book-list'),
 ]
