407fb0ef207cc4a5e2edacb5c678382b9d2d475d
N-Hosana <hosanah08@gmail.com>
2024-11-20 00:55:32 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import BookList
 
 urlpatterns = [
+    api.urls
+    rest_framework.generics.ListAPIView
     path('books/', BookList.as_view(), name='book-list'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
