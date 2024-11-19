d4b11e828f61751a2488df159da5fca72a64eca0
N-Hosana <hosanah08@gmail.com>
2024-11-20 01:04:21 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,5 +5,5 @@ from .views import BookList
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Ensure this line includes `api.urls`
-    path('books/', BookList.as_view(), name='book-list'),
+   path('books/', BookList.as_view(), name='book-list'),
 ]
