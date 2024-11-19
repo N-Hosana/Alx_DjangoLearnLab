f242c89ac72677a3601921649e7940ee3cab78f7
N-Hosana <hosanah08@gmail.com>
2024-11-20 00:46:38 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,11 +1,6 @@
-# api/views.py
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
-
-
-
-
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
