8ab57c7527221361ceadbf528a45ccd37bfb656b
N-Hosana <hosanah08@gmail.com>
2024-11-19 16:27:44 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,34 +1,16 @@
-from relationship_app.models import Author, Book, Library, Librarian
+ifrom relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-def query_books_by_author(author_name):
-    books = Book.objects.filter(author__name__icontains=author_name)
-    return books
+def get_books_by_author(author_name):
+     author = Author.objects.get(name=author_name)
+    return Book.objects.filter(author=author)
 
 # List all books in a library
-def list_books_in_library(library_name):
-    books = Book.objects.filter(library__name__icontains=library_name)
-    return books
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
 # Retrieve the librarian for a library
-def retrieve_librarian_for_library(library_name):
-    librarian = Librarian.objects.filter(library__name__icontains=library_name).first()
-    return librarian
-
-
-# Example usage
-if __name__ == "__main__":
-    # Query all books by "J.K. Rowling"
-    print("Books by J.K. Rowling:")
-    for book in query_books_by_author("J.K. Rowling"):
-        print(book.title)
-
-    # List all books in "Central Library"
-    print("\nBooks in Central Library:")
-    for book in list_books_in_library("Central Library"):
-        print(book.title)
-
-    # Retrieve the librarian for "Central Library"
-    print("\nLibrarian for Central Library:")
-    librarian = retrieve_librarian_for_library("Central Library")
-    print(librarian.name if librarian else "No librarian found")
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
