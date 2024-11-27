e91b36588364a2ad29137498cf10d934ddba8aac
N-Hosana <hosanah08@gmail.com>
2024-11-27 03:23:37 -0800

    update views.py

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# api/views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Author
+from .serializers import AuthorSerializer
 
-# Create your views here.
+class AuthorListView(APIView):
+    def get(self, request):
+        authors = Author.objects.all()
+        serializer = AuthorSerializer(authors, many=True)
+        return Response(serializer.data)
+
