571df46eb64073edcd3ef32a6c18a7ab9b260888
N-Hosana <hosanah08@gmail.com>
2024-12-12 23:11:44 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('tags/<slug:tag_slug>', PostByTagListView.as_view),
 
     # Comment paths
     path('posts/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
