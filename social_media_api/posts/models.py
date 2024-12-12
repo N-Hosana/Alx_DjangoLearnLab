c7a20948513e44287eacf1137743f4437fcfc586
N-Hosana <hosanah08@gmail.com>
2024-12-13 01:51:35 +0200

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentTyp
 from django.conf import settings
 
 class Like(models.Model):
@@ -8,6 +10,19 @@ class Like(models.Model):
 
     class Meta:
         unique_together = ('user', 'post')  # Prevent duplicate likes
+
+class Notification(models.Model):
+    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='actor')
+    verb = models.CharField(max_length=255)  # e.g., 'liked your post'
+    target_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    target_object_id = models.PositiveIntegerField(null=True)
+    target = GenericForeignKey('target_content_type', 'target_object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.actor} {self.verb} {self.target}"
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
