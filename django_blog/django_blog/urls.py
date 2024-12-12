8b7e1b0310e366507db8317aac617451fd6b34b9
N-Hosana <hosanah08@gmail.com>
2024-12-12 22:12:47 +0200

    Update urls.py

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path
+from . import views
+
+
+app_name = 'blog'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
