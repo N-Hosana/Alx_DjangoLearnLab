ec1405fa32a2c244ca621ff92efa2c4ef520982c
N-Hosana <hosanah08@gmail.com>
2024-12-13 02:30:44 +0200

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -66,6 +66,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL', f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+                'ENGINE': 'django.db.backends.postgresql',  # Or 'django.db.backends.mysql'
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port is 5432
     )
 }
 
