19a5b97669316430b9b1012db493c99848d4b474
N-Hosana <hosanah08@gmail.com>
2024-12-16 18:41:57 +0200

    Update models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class User(AbstractUser):
     following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
 
     def __str__(self):
         return self.username
